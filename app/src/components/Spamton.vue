5a725ce3ec190cfee7fcb61f1ae2be6978051484
lucyk22 <lucy.kwan22@sitechhs.com>
2021-10-14 12:29:04 -0400

    spamtoning

---
+++ app/src/components/Spamton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <h1>HI   EVERY  !</h1>
+    <div class="spamton">
+      <img src="../assets/spamton.png" alt="">
+    </div>
+    <div class="dialogue-box">
+      <p class="dialogue-text"> {{dialogue}}</p>
+    </div>
+    <div class="  "></div>
+  </div>
+</template>
+
+<script>
+ export default {
+  name: 'Spamton',
+  props: {
+    dialogue: String
+  }
+}
+</script>
+
+
